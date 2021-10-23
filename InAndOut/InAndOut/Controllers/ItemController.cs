87f8b03a185ff30dc3837fd611d100b4921b4256
stalg94 <stalg28@gmail.com>
2021-10-24 00:03:01 +0300

    passing data to the controller

--- InAndOut/InAndOut/Controllers/ItemController.cs
+++ InAndOut/InAndOut/Controllers/ItemController.cs
@@ -3,14 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InAndOut.Models;
+using InAndOut.Data;
+
 
 namespace InAndOut.Controllers
 {
     public class ItemController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Item> objectList = _db.Items;
+            return View(objectList);
         }
     }
 }
